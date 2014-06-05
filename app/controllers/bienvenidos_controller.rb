fd82be03cc58fc7aa655ce5a5528aa938a8c4155
alpa84 <alejandrofpanza@gmail.com>
2014-06-05 14:12:08 -0300

    oprimized previous 1 + n queries
    Former-commit-id: 728f05f7d1aeae1bd3dc80ac7bc3d7ede08c360d

--- app/controllers/bienvenidos_controller.rb
+++ app/controllers/bienvenidos_controller.rb
@@ -32,62 +32,73 @@ LEGEND_SAMPLES = 5
  
     @gradient = Gradient.new( LOW_COLOR, HIGH__COLOR, SAMPLES)
     @all_circles = circulos_de_intensidad({'cargo_id' => @cargo_id, 'partido_id' => @partido_id})
-    @all_circles_map = alt_map(:container_id => "map_all",
+    @all_circles_map = AltLeaflet.alt_map(:container_id => "map_all",
     :center => {:latlng => [-32.954088, -60.664458],:zoom => 12 },
     :circles =>  @all_circles[:leaflet])
 
     @fein_circles = circulos_de_intensidad({'cargo_id' => 4, 'partido_id' => 4})
-    @fein_circles_map = alt_map(:container_id => "map_fein",
+    @fein_circles_map = AltLeaflet.alt_map(:container_id => "map_fein",
     :center => {:latlng => [-32.954088, -60.664458],:zoom => 12 },
     :circles =>  @fein_circles[:leaflet])
 
     @miguel_circles = circulos_de_intensidad({'cargo_id' => 2, 'partido_id' => 4})
-    @miguel_circles_map = alt_map(:container_id => "map_mig",
+    @miguel_circles_map = AltLeaflet.alt_map(:container_id => "map_mig",
     :center => {:latlng => [-32.954088, -60.664458],:zoom => 12 },
     :circles =>  @miguel_circles[:leaflet])
 
     @castells_circles = circulos_de_intensidad({'cargo_id' => 1, 'partido_id' => 5})
-    @castells_circles_map = alt_map(:container_id => "map_castells",
+    @castells_circles_map = AltLeaflet.alt_map(:container_id => "map_castells",
     :center => {:latlng => [-32.94,-60.66],:zoom => 13 },
     :circles =>  @castells_circles[:leaflet])
 
     @pro_circles = circulos_de_intensidad({'partido_id' => 2})
-    @pro_map = alt_map(:container_id => "map_pro",
+    @pro_map = AltLeaflet.alt_map(:container_id => "map_pro",
     :center => {:latlng => [-32.954088, -60.664458],:zoom => 12 },
     :circles =>  @pro_circles[:leaflet])
 
     @socialismo_circles = circulos_de_intensidad({'partido_id' => 4})
-    @socialismo_circles_map = alt_map(:container_id => "map_soc",
+    @socialismo_circles_map = AltLeaflet.alt_map(:container_id => "map_soc",
     :center => {:latlng => [-32.954088, -60.664458],:zoom => 12 },
     :circles =>  @socialismo_circles[:leaflet])
 
     @k_circles = circulos_de_intensidad({'partido_id' => 3})
-    @k_circles_map = alt_map(:container_id => "map_k",
+    @k_circles_map = AltLeaflet.alt_map(:container_id => "map_k",
     :center => {:latlng => [-32.954088, -60.664458],:zoom => 12 },
     :circles =>  @k_circles[:leaflet])
   end
 end
 
 def circulos_de_intensidad(selected = {'partido_id' => 1})
-  votes_for_party = VotesTotal.where(political_party_id:selected['partido_id'])
-  leaflet_without_interpolation = School.all.collect do |school|
-    if selected['cargo_id'] and selected['cargo_id'] != 't'
-      votes = votes_for_party.where(public_office_id:selected['cargo_id']).where(school_id:school.id)[0].votes
-      total = school.total
-    else
-      votes = 0
-      all_offices_votes = votes_for_party.where(school_id:school.id).each do |office_votes|
-        votes += office_votes.votes
-      end
-      total = school.total * 5
-    end
-    lat = school.lat
-    lon = school.lon
-    ratio = (votes.to_f / total)
-    popup = "<b>#{(ratio*100).round(2)}%</b> de este local electoral, <br><b>#{votes}</b> votos de un total de <b>#{total}</b>, <br>#{school.name} ".gsub(/[°()\'\"]/i, '') 
-    leaflet_circles({'ratio' => ratio, 'lat' => lat, 'lon' => lon, 'popup' => popup })
 
+  if selected['cargo_id'] and selected['cargo_id'] != 't'
+    #votes_schools = VotesTotal.joins(:school).where(political_party_id:selected['partido_id'], public_office_id:selected['cargo_id'])
+    votes_schools = VotesTotal.includes(:school).where(political_party_id:selected['partido_id'], public_office_id:selected['cargo_id']).references(:school)
+    leaflet_without_interpolation = votes_schools.collect do |vote_school|
+      votes = vote_school.votes
+      total = vote_school.school.total
+      lat = vote_school.school.lat
+      lon = vote_school.school.lon
+      ratio = (votes.to_f / total)
+      school_name = vote_school.school.name
+      popup = "<b>#{(ratio*100).round(2)}%</b> de este local electoral, <br><b>#{votes}</b> votos de un total de <b>#{total}</b>, <br>#{school_name} ".gsub(/[°()\'\"]/i, '') 
+      leaflet_circles({'ratio' => ratio, 'lat' => lat, 'lon' => lon, 'popup' => popup })
+    end
+  else
+    parties_partials = VotesTotal.includes(:school).where(political_party_id:selected['partido_id']).references(:school).group(:school_id).sum(:votes)
+    #parties_partials = VotesTotal.joins(:school).where(political_party_id:selected['partido_id']).group(:school_id).sum(:votes)
+    schools = School.all
+    leaflet_without_interpolation = schools.collect do |school|
+      votes = parties_partials[school.id]
+      total = school.total * 5
+      lat = school.lat
+      lon = school.lon
+      ratio = (votes.to_f / total)
+      popup = "<b>#{(ratio*100).round(2)}%</b> de este local electoral, <br><b>#{votes}</b> votos de un total de <b>#{total}</b>, <br>#{school.name} ".gsub(/[°()\'\"]/i, '') 
+      leaflet_circles({'ratio' => ratio, 'lat' => lat, 'lon' => lon, 'popup' => popup })
+
+    end
   end
+  
   leaflet = interpolate_range(leaflet_without_interpolation, 100)
   labels = labels_a({:min => leaflet[:min], :max => leaflet[:max]})
   { :leaflet => leaflet[:circles], :labels => labels}
@@ -125,84 +136,3 @@ def interpolate_range(circles, samples)
   {:circles => with_proper_colors, :max => max, :min => min}
 end
 
-
-#-------------------------
-
-
-def alt_map(options)
-  options[:tile_layer] ||= Leaflet.tile_layer
-  options[:attribution] ||= Leaflet.attribution
-  options[:max_zoom] ||= Leaflet.max_zoom
-  options[:container_id] ||= 'map'
-
-  output = []
-  output << "<div id='#{options[:container_id]}'></div>" unless options[:no_container]
-  output << "<script>"
-  output << "var map = L.map('#{options[:container_id]}')"
-  
-  if options[:custom_marker]
-    output << "var greenIcon = L.icon({
-    iconUrl: 'leaf-green.png',
-    shadowUrl: 'leaf-shadow.png',
-
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-    });"
-
-  end
-
-  if options[:center]
-    output << "map.setView([#{options[:center][:latlng][0]}, #{options[:center][:latlng][1]}], #{options[:center][:zoom]})"
-  end
-
-
-  if options[:markers]
-    options[:markers].each do |marker|
-      output << "marker = L.marker([#{marker[:latlng][0]}, #{marker[:latlng][1]}]"
-      if options[:custom_marker]
-        output.last << ", {icon: greenIcon}" 
-      end
-      output.last << ").addTo(map)"
-      if marker[:popup]
-        output << "marker.bindPopup('#{marker[:popup]}')"
-      end
-    end
-  end
-
-  if options[:circles]
-    options[:circles].each do |circle|
-      output << "L.circle(['#{circle[:latlng][0]}', '#{circle[:latlng][1]}'], #{circle[:radius]}, {
-        color: '#{circle[:color]}',
-        fillColor: '#{circle[:fillColor]}',
-        fillOpacity: #{circle[:fillOpacity]}
-        }"
-      if circle[:popup]
-         output.last << ").bindPopup(String('#{circle[:popup]}'))"
-      end
-      output.last << '.addTo(map);'
-    end
-  end
-
-  if options[:polylines]
-     options[:polylines].each do |polyline|
-       _output = "L.polyline(#{polyline[:latlngs]}"
-       _output << "," + polyline[:options].to_json if polyline[:options]
-       _output << ").addTo(map);"
-       output << _output.gsub(/\n/,'')
-     end
-  end
-
-  if options[:fitbounds]
-    output << "map.fitBounds(L.latLngBounds(#{options[:fitbounds]}));"
-  end
-
-  output << "L.tileLayer('#{options[:tile_layer]}', {
-attribution: '#{options[:attribution]}',
-maxZoom: #{options[:max_zoom]}
-}).addTo(map)"
-  output << "</script>"
-  output.join("\n").html_safe
-end
